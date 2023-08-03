6013f2ca946b093c77d195e5c600024a78210272
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-03 10:01:01 +0300

    Add spinner
    Using narrowing type handling

--- demo-ts/src/components/People.tsx
+++ demo-ts/src/components/People.tsx
@@ -1,15 +1,21 @@
-import { usePeople } from "../hooks/usePeople";
+import { useState } from "react";
+import { LoadStateType, usePeople } from "../hooks/usePeople";
 import Person from "./Person";
+import Spinner from "./Spinner";
 
 const People = () => {
-  const people = usePeople();
+  const [isLoading, setIsLoading] = useState<LoadStateType>(false);
 
+  const changeIsLoading = (state: LoadStateType) => {
+    setIsLoading(state);
+  };
+  const people = usePeople(changeIsLoading);
   return (
     <>
       {people.map((person) => (
         <Person key={person.url} person={person} />
       ))}
-      {people.length === 0 && <p>Loading...</p>}
+      {isLoading && <Spinner />}
     </>
   );
 };
--- demo-ts/src/hooks/usePeople.ts
+++ demo-ts/src/hooks/usePeople.ts
@@ -19,13 +19,28 @@ export type PersonType = {
   url: string;
 };
 
-export const usePeople = (): PersonType[] => {
+export type LoadStateType = false | true;
+type changeIsLoadingType = (value: LoadStateType) => void;
+
+export const usePeople = (
+  changeIsLoading: changeIsLoadingType
+): PersonType[] => {
   const [people, setPeople] = useState<PersonType[]>([]);
 
   const fetchPeople = async () => {
-    const response = await fetch("https://swapi.dev/api/people");
-    const data = await response.json();
-    setPeople(data.results);
+    try {
+      changeIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/people");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      setPeople(data.results);
+      changeIsLoading(false);
+    } catch (e) {
+      changeIsLoading(false);
+      throw new Error(e);
+    }
   };
 
   useEffect(() => {
